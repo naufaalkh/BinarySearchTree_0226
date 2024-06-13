1fa643dd0d2a24a5769ca3930db132557d5eb349
naufaalkh <naufaalkh@gmail.com>
2024-06-13 15:43:17 +0700

    membuat void inorder

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -61,6 +61,18 @@ public:
         currentNode = ROOT;
         parent = NULL;
         while((currentNode != NULL ) && (currentNode->info != element))
-        {}
+        {
+            parent = currentNode;
+            if (element < currentNode->info)
+                currentNode = currentNode->leftchild;
+            else
+            currentNode = currentNode->rightchild;
+        }
     }
+
+    void inorder(Node* ptr)
+    {
+        
+    }
+
 };
