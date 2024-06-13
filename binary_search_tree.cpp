e5175079fb785925e57bbd40a5c115aa18f91684
naufaalkh <naufaalkh@gmail.com>
2024-06-13 15:14:28 +0700

    membuat constructor

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -9,5 +9,11 @@ public:
     Node* leftchild;
     Node* rightchild;
 
-    
+    // Constructor for the node class
+    Node (string i, Node* l, Node* r)
+    {
+        info = 1;
+        leftchild = l;
+        rightchild = r;
+    }
 };
